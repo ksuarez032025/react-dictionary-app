c4981bfb022bb090e3c3ea341a75645ad648ea1a
ksuarez032025 <kbsuarez626@gmail.com>
2025-08-25 15:13:40 -0400

    Add axios and integrate API call in Dictionary
    Added axios as a dependency and updated Dictionary.js to fetch word definitions from the SheCodes API using axios. Also removed the apple-touch-icon link from index.html for cleanup.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    const apiKey = "c3650d7d0ad3c75tcafd67c27c4o8bd0";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
