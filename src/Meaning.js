7537348a08e2fcf4fe49305f56c353391f0a07f5
ksuarez032025 <kbsuarez626@gmail.com>
2025-08-25 16:34:28 -0400

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,11 +4,15 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definition}
-        <br />
-        <em>{props.meaning.example}</em>
-      </p>
+      <div>
+        <p>
+          <strong>Definition: </strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Example: </strong>
+          <em>{props.meaning.example}</em>
+        </p>
+      </div>
     </div>
   );
 }
