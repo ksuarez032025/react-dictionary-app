7dcb1309e4b6c3448ee8f5831c05c23991bace67
ksuarez032025 <kbsuarez626@gmail.com>
2025-08-26 16:06:49 -0400

    Integrate Pexels API for photo search
    Added the Pexels API client and photo search functionality to Dictionary.js, displaying related images for searched keywords. Updated dependencies to include pexels package. Minor CSS adjustments for definition and synonyms font sizes.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import { createClient } from "pexels";
 import "./Dictionary.css";
+
+const pexelsClient = createClient(process.env.REACT_APP_PEXELS_KEY);
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("hello");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState([]);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
   }
 
   function search() {
-    // API Documentation: https://www.shecodes.io/learn/apis/dictionary
+    // SheCodes Dictionary API Documentation: https://www.shecodes.io/learn/apis/dictionary
     const apiKey = "c3650d7d0ad3c75tcafd67c27c4o8bd0";
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // Pexels API Documentation: https://www.pexels.com/api/documentation/?#photos-search
+    pexelsClient.photos
+      .search({ query: keyword, per_page: 6 })
+      .then((res) => setPhotos(res.photos))
+      .catch(console.error);
   }
 
   function handleSubmit(event) {
@@ -46,7 +56,18 @@ export default function Dictionary() {
             />
           </form>
         </section>
+
         <Results results={results} />
+
+        {photos.length > 0 && (
+          <section className="photos">
+            {photos.map((p) => (
+              <a key={p.id} href={p.url} target="blank" rel="noreferrer">
+                <img src={p.src.medium} alt={p.alt || keyword} />
+              </a>
+            ))}
+          </section>
+        )}
       </div>
     );
   } else {
